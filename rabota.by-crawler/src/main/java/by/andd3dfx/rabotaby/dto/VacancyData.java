bc992f3e347fa58b3ff86c9a21c60c24eca28e98
Andrei Punko <andd3dfx@gmail.com>
2024-11-11 17:55:35 +0300

    Use toString() generated by Lombok instead of custom. Ignore null fields during serialization

--- rabota.by-crawler/src/main/java/by/andd3dfx/rabotaby/dto/VacancyData.java
+++ rabota.by-crawler/src/main/java/by/andd3dfx/rabotaby/dto/VacancyData.java
@@ -1,11 +1,13 @@
 package by.andd3dfx.rabotaby.dto;
 
-import java.util.Set;
-
 import by.andd3dfx.crawler.dto.CrawlerData;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Set;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder
 @Data
 public class VacancyData implements CrawlerData {
@@ -15,12 +17,4 @@ public class VacancyData implements CrawlerData {
     private String textContent;
     private Set<String> keywords;
     private String addressString;
-
-    @Override
-    public String toString() {
-        return "VacancyData{" +
-            "url='" + url + '\'' +
-            ", keywords=" + keywords +
-            '}';
-    }
 }
