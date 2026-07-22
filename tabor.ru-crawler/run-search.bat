161356846d7a9be858c55656019f77fcc3f5e58d
Andrei Punko <andd3dfx@gmail.com>
2026-07-22 21:43:46 +0300

    Adjust default values in script

--- tabor.ru-crawler/run-search.bat
+++ tabor.ru-crawler/run-search.bat
@@ -2,4 +2,4 @@
 chcp 65001 >nul
 rem Resume mode: keep profiles.json and photos/, skip already known ids
 rem args: outputJson [pagesCap] [delayMs] [minAge] [maxAge]
-call java -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -jar target/tabor.ru-crawler-1.0-SNAPSHOT-jar-with-dependencies.jar profiles.json -1 250 25 45
+call java -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -jar target/tabor.ru-crawler-1.0-SNAPSHOT-jar-with-dependencies.jar profiles.json -1 250 30 45
